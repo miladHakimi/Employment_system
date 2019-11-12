8258ae84d3a3e758f1d49d764de31f589080fa0d
miladHakimi <miladhakimi33@gmail.com>
2019-11-12 23:38:17 +0330

    Applicant added

---
+++ Employment_app/Users/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+
+
+class Applicant(AbstractBaseUser):
+    firstName = models.CharField(
+        _('first name'),
+        max_length=50
+    )
+    lastName = models.CharField(
+        _('last name'),
+        max_length=50
+    )
+    userName = models.CharField(
+        _('user name'),
+        max_length=50
+    )
+    Age = models.IntegerField(
+        _('Age'),
+        default=18,
+        blank=True,
+    )
+    GENDER_CHOICES = ['male', 'female']
+    gender = models.CharField(
+        choices=GENDER_CHOICES,
+    )
+    # todo: verify phone
+    phone = models.CharField(
+        max_length=11,
+        null=True,
+        blank=True,
+    )
