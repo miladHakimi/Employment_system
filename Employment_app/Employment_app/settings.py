67c2fd2c2164e3cd56e9f86f42dc928b2658a07d
miladHakimi <miladhakimi33@gmail.com>
2019-11-13 10:17:41 +0330

    rest framework added to settings

--- Employment_app/Employment_app/settings.py
+++ Employment_app/Employment_app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Accounting.apps.AccountingConfig',
+    'Employment_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
